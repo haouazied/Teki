fcb9135d650836e3bd5a8305c2d137612ae94776
Zied Haoua <ziedhaoua@gmail.com>
2019-01-23 10:56:55 +0100

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by MacBook on 1/23/19.
+//  Copyright © 2019 Haoua Zied. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
